8bef2ab1854a3da2526b6082363e5253230c9196
Hachenda <dhernandezla0@gmail.com>
2023-06-21 21:45:38 +0200

    Add SASS and styles

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -3,32 +3,32 @@
 @section('title', __('web.title'))
 
 @section('content')
-    <h1>{{__('home.title')}}</h1>
-    <h2>{{$planet->name}}</h2>
-    <div id="resources">
-        <h2>{{__('metal')}}: <span id="metal">{{intval($Usergame->metal)}}</span></h2>
-        <h2>{{__('crystal')}}: <span id="crystal">{{intval($Usergame->crystal)}}</span></h2>
-        <h2>{{__('deuterium')}}: <span id="deuterium">{{intval($Usergame->deuterium)}}</span></h2>
+    <h1 class="game-title">{{__('web.title')}}</h1>
+    <h2 class="planet-name">{{$planet->name}}</h2>
+    <div id="resources" class="resources-container">
+        <h2 class="resource">{{__('metal')}}: <span id="metal">{{intval($Usergame->metal)}}</span></h2>
+        <h2 class="resource">{{__('crystal')}}: <span id="crystal">{{intval($Usergame->crystal)}}</span></h2>
+        <h2 class="resource">{{__('deuterium')}}: <span id="deuterium">{{intval($Usergame->deuterium)}}</span></h2>
     </div>
 
-    <hr>
+    <hr class="separator">
     @foreach ($buildingPlanets as $buildingPlanet)
-    <h4>{{ $buildingPlanet->building->getTranslation('name', config('app.locale')) }}</h4>
-    <h4>{{ $buildingPlanet->building->getTranslation('description', config('app.locale')) }}</h4>
+        <h4 class="building-name">{{ $buildingPlanet->building->getTranslation('name', config('app.locale')) }}</h4>
+        <h4 class="building-description">{{ $buildingPlanet->building->getTranslation('description', config('app.locale')) }}</h4>
 
-    @foreach ($buildingLevels as $buildingLevel)
-        @if ($buildingLevel->building_id === $buildingPlanet->building->id && $buildingLevel->level === $buildingPlanet->level)
-            <p>{{__('Level')}}: {{ $buildingLevel->level }}</p>
-            <p>{{__('metal_cost')}}: {{ $buildingLevel->metal_cost }}</p>
-            <p>{{__('crystal_cost')}}: {{ $buildingLevel->crystal_cost }}</p>
-            <p>{{__('deuterium_cost')}}: {{ $buildingLevel->deuterium_cost }}</p>
-            {{-- <p>Energy Cost: {{ $buildingLevel->energy_cost }}</p> --}}
-            {{-- <p>Production Rate: {{ $buildingLevel->production_rate }}</p> --}}
-            {{-- <p>Resource Type: {{ $buildingLevel->resource_type }}</p> --}}
-        @endif
+        @foreach ($buildingLevels as $buildingLevel)
+            @if ($buildingLevel->building_id === $buildingPlanet->building->id && $buildingLevel->level === $buildingPlanet->level)
+                <p class="building-level">{{__('Level')}}: {{ $buildingLevel->level }}</p>
+                <p class="building-cost">{{__('metal_cost')}}: {{ $buildingLevel->metal_cost }}</p>
+                <p class="building-cost">{{__('crystal_cost')}}: {{ $buildingLevel->crystal_cost }}</p>
+                <p class="building-cost">{{__('deuterium_cost')}}: {{ $buildingLevel->deuterium_cost }}</p>
+                {{-- <p class="building-energy-cost">{{__('Energy Cost')}}: {{ $buildingLevel->energy_cost }}</p> --}}
+                {{-- <p class="building-production-rate">{{__('Production Rate')}}: {{ $buildingLevel->production_rate }}</p> --}}
+                {{-- <p class="building-resource-type">{{__('Resource Type')}}: {{ $buildingLevel->resource_type }}</p> --}}
+            @endif
         @endforeach
-        <button>{{__('update_building')}}</button>
+        <button class="update-building-button">{{__('update_building')}}</button>
 
-    <hr>
-@endforeach
+        <hr class="separator">
+    @endforeach
 @endsection
