86ae11e41eed084c4e114bd2be35169c383f5074
Hachenda <dhernandezla0@gmail.com>
2023-07-16 13:53:49 +0200

    Arrow functions, negative numbers input fix

--- public/js/app.js
+++ public/js/app.js
@@ -22,7 +22,7 @@ if (energyValue < 0) {
 
 setTimeout(() => {
     let alerts = document.getElementsByClassName('alert');
-    for (var i = 0; i < alerts.length; i++) {
+    for (let i = 0; i < alerts.length; i++) {
         alerts[i].style.display = 'none';
     }
 }, 10000);
@@ -69,50 +69,64 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 document.addEventListener('DOMContentLoaded', () => {
-    const shipNumberInput = $('input[name="ship_number"]');
+    const shipNumberInputs = document.querySelectorAll('input[name="ship_number"]');
 
-    shipNumberInput.on('input', () => {
-        let shipNumber = parseInt(shipNumberInput.val());
+    shipNumberInputs.forEach((shipNumberInput) => {
+        shipNumberInput.addEventListener('input', () => {
+            let shipNumber = parseInt(shipNumberInput.value);
 
-        if (shipNumber <= 0) {
-            shipNumberInput.val(1);
-        }
+            if (shipNumber <= 0) {
+                shipNumberInput.value = 1;
+            }
+        });
+    });
+
+    const defenseNumberInputs = document.querySelectorAll('input[name="defense_number"]');
+
+    defenseNumberInputs.forEach((defenseNumberInput) => {
+        defenseNumberInput.addEventListener('input', () => {
+            let defenseNumber = parseInt(defenseNumberInput.value);
+
+            if (defenseNumber <= 0) {
+                defenseNumberInput.value = 1;
+            }
+        });
     });
 });
 
 const spy_arrival = document.getElementById('spy_arrival');
 let arrival_coordinates = document.getElementById('arrival_coordinates');
 if (spy_arrival && arrival_coordinates) {
-        arrival_coordinates = arrival_coordinates.innerText;
-        arrival_coordinates = arrival_coordinates.match(/\d+/g);
+    arrival_coordinates = arrival_coordinates.innerText;
+    arrival_coordinates = arrival_coordinates.match(/\d+/g);
 
-        const arrival_time = spy_arrival.innerText;
-        
-            function actualizarSpy() {
-                let fechaActual = new Date();
-                
-                let year = fechaActual.getFullYear();
-                let month = (fechaActual.getMonth() + 1).toString().padStart(2, '0');
-                let day = fechaActual.getDate().toString().padStart(2, '0');
-                let hours = fechaActual.getHours().toString().padStart(2, '0');
-                let minutes = fechaActual.getMinutes().toString().padStart(2, '0');
-                let seconds = fechaActual.getSeconds().toString().padStart(2, '0');
-                
-                let now = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
-                
-                let timeDifference = -(new Date(now) - new Date(arrival_time)) / 1000; // Convertir a segundos
-                
-                let hoursDiff = Math.floor(timeDifference / 3600);
-                let minutesDiff = Math.floor((timeDifference % 3600) / 60);
-                let secondsDiff = Math.floor(timeDifference % 60);
-                
-                spy_arrival.style.display = "initial";
-                spy_arrival.innerText = `${hoursDiff}h ${minutesDiff}m ${secondsDiff}s`;
-                
-                if (timeDifference <= 0) {
-                    $.ajax({
-                        url: '/notification-spy',
-                        type: 'POST',
+    const arrival_time = spy_arrival.innerText;
+
+    const actualizarSpy = () => {
+        let fechaActual = new Date();
+
+        let year = fechaActual.getFullYear();
+        let month = (fechaActual.getMonth() + 1).toString().padStart(2, '0');
+        let day = fechaActual.getDate().toString().padStart(2, '0');
+        let hours = fechaActual.getHours().toString().padStart(2, '0');
+        let minutes = fechaActual.getMinutes().toString().padStart(2, '0');
+        let seconds = fechaActual.getSeconds().toString().padStart(2, '0');
+
+        let now = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+
+        let timeDifference = -(new Date(now) - new Date(arrival_time)) / 1000; // Convertir a segundos
+
+        let hoursDiff = Math.floor(timeDifference / 3600);
+        let minutesDiff = Math.floor((timeDifference % 3600) / 60);
+        let secondsDiff = Math.floor(timeDifference % 60);
+
+        spy_arrival.style.display = "initial";
+        spy_arrival.innerText = `${hoursDiff}h ${minutesDiff}m ${secondsDiff}s`;
+
+        if (timeDifference <= 0) {
+            $.ajax({
+                url: '/notification-spy',
+                type: 'POST',
                 data: {
                     _token: $('meta[name="csrf-token"]').attr('content'),
                     ssp_otherPlanet: arrival_coordinates[0],
@@ -127,38 +141,39 @@ if (spy_arrival && arrival_coordinates) {
             });
             location.reload();
         }
-    }
+    };
+
     setInterval(actualizarSpy, 1000);
 }
 
 document.addEventListener('DOMContentLoaded', () => {
     const accordionItems = document.querySelectorAll('.accordion-item');
-  
-    accordionItems.forEach(function(item) {
-      const header = item.querySelector('.accordion-header h3');
-      const content = item.querySelector('.accordion-content');
-  
-      header.addEventListener('click', function() {
-        item.classList.toggle('active');
-        if (item.classList.contains('active')) {
-          content.style.maxHeight = content.scrollHeight + 'px';
-        } else {
-          content.style.maxHeight = '0';
-        }
-      });
-    });
-  });
 
-  document.addEventListener('DOMContentLoaded', () => {
+    accordionItems.forEach(item => {
+        const header = item.querySelector('.accordion-header h3');
+        const content = item.querySelector('.accordion-content');
+
+        header.addEventListener('click', () => {
+            item.classList.toggle('active');
+            if (item.classList.contains('active')) {
+                content.style.maxHeight = content.scrollHeight + 'px';
+            } else {
+                content.style.maxHeight = '0';
+            }
+        });
+    });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
     const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
     const deleteButtons = document.querySelectorAll('.fas.fa-times');
     const notifications = document.querySelectorAll('.accordion-header.unread');
 
-    
-    deleteButtons.forEach(function (button) {
+
+    deleteButtons.forEach(button => {
         button.addEventListener('click', () => {
-            const notificationId = this.getAttribute('data-notification-id');
-            const notificationItem = this.closest('.accordion-item');
+            const notificationId = button.getAttribute('data-notification-id');
+            const notificationItem = button.closest('.accordion-item');
 
             fetch('/notifications/' + notificationId, {
                 method: 'DELETE',
@@ -167,23 +182,23 @@ document.addEventListener('DOMContentLoaded', () => {
                     'Content-Type': 'application/json'
                 },
             })
-            .then(function (response) {
+            .then(response => {
                 if (response.ok) {
                     notificationItem.remove();
                 } else {
                     throw new Error('Error while deleting notification');
                 }
             })
-            .catch(function (error) {
+            .catch(error => {
                 console.error(error);
             });
         });
     });
 
-    notifications.forEach(function (notification) {
+    notifications.forEach(notification => {
         notification.addEventListener('click', () => {
-            const notificationId = this.getAttribute('data-notification-id');
-            
+            const notificationId = notification.getAttribute('data-notification-id');
+
             fetch('/notification-read/' + notificationId, {
                 method: 'POST',
                 headers: {
@@ -191,7 +206,7 @@ document.addEventListener('DOMContentLoaded', () => {
                     'Content-Type': 'application/json'
                 },
             })
-            .catch(function (error) {
+            .catch(error => {
                 console.error(error);
             });
         });
