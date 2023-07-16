1d9b194af3fc12693597202c199b20979943065f
Hachenda <dhernandezla0@gmail.com>
2023-07-16 14:04:18 +0200

    Optimize js

--- public/js/app.js
+++ public/js/app.js
@@ -1,20 +1,40 @@
+const metalElement = document.getElementById('metal');
+const crystalElement = document.getElementById('crystal');
+const deuteriumElement = document.getElementById('deuterium');
+const energyElement = document.getElementById('energy');
+
+const planetListName = document.getElementById('planetListName');
+const planetName = document.getElementById('planetName');
+const editIcon = document.getElementById('editIcon');
+const editInput = document.getElementById('editInput');
+
+const shipNumberInputs = document.querySelectorAll('input[name="ship_number"]');
+const defenseNumberInputs = document.querySelectorAll('input[name="defense_number"]');
+
+let spyArrival = document.getElementById('spy_arrival');
+let arrivalCoordinates = document.getElementById('arrival_coordinates');
+const accordionItems = document.querySelectorAll('.accordion-item');
+const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+const deleteButtons = document.querySelectorAll('.fas.fa-times');
+const notifications = document.querySelectorAll('.accordion-header.unread');
+
+
+
 function updateResources() {
     fetch('/update-resources')
         .then(response => response.json())
         .then(data => {
             const { metal, crystal, deuterium, energy } = data;
-            document.getElementById('metal').textContent = Math.floor(metal);
-            document.getElementById('crystal').textContent = Math.floor(crystal);
-            document.getElementById('deuterium').textContent = Math.floor(deuterium);
-            document.getElementById('energy').textContent = Math.floor(energy);
+            metalElement.textContent = Math.floor(metal);
+            crystalElement.textContent = Math.floor(crystal);
+            deuteriumElement.textContent = Math.floor(deuterium);
+            energyElement.textContent = Math.floor(energy);
         })
         .catch(error => console.log(error));
 }
 
 setInterval(updateResources, 5000);
 
-
-const energyElement = document.getElementById('energy');
 const energyValue = parseInt(energyElement.textContent);
 if (energyValue < 0) {
     energyElement.style.color = 'red';
@@ -29,11 +49,6 @@ setTimeout(() => {
 
 document.addEventListener('DOMContentLoaded', () => {
     const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
-
-    const planetListName = document.getElementById('planetListName');
-    const planetName = document.getElementById('planetName');
-    const editIcon = document.getElementById('editIcon');
-    const editInput = document.getElementById('editInput');
 
     editIcon.addEventListener('click', () => {
         editInput.value = planetName.innerText;
@@ -66,9 +81,7 @@ document.addEventListener('DOMContentLoaded', () => {
             .catch(error => console.error(error));
         }
     });
-});
 
-document.addEventListener('DOMContentLoaded', () => {
     const shipNumberInputs = document.querySelectorAll('input[name="ship_number"]');
 
     shipNumberInputs.forEach((shipNumberInput) => {
@@ -92,61 +105,7 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         });
     });
-});
-
-const spy_arrival = document.getElementById('spy_arrival');
-let arrival_coordinates = document.getElementById('arrival_coordinates');
-if (spy_arrival && arrival_coordinates) {
-    arrival_coordinates = arrival_coordinates.innerText;
-    arrival_coordinates = arrival_coordinates.match(/\d+/g);
-
-    const arrival_time = spy_arrival.innerText;
-
-    const actualizarSpy = () => {
-        let fechaActual = new Date();
-
-        let year = fechaActual.getFullYear();
-        let month = (fechaActual.getMonth() + 1).toString().padStart(2, '0');
-        let day = fechaActual.getDate().toString().padStart(2, '0');
-        let hours = fechaActual.getHours().toString().padStart(2, '0');
-        let minutes = fechaActual.getMinutes().toString().padStart(2, '0');
-        let seconds = fechaActual.getSeconds().toString().padStart(2, '0');
-
-        let now = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
-
-        let timeDifference = -(new Date(now) - new Date(arrival_time)) / 1000; // Convertir a segundos
-
-        let hoursDiff = Math.floor(timeDifference / 3600);
-        let minutesDiff = Math.floor((timeDifference % 3600) / 60);
-        let secondsDiff = Math.floor(timeDifference % 60);
-
-        spy_arrival.style.display = "initial";
-        spy_arrival.innerText = `${hoursDiff}h ${minutesDiff}m ${secondsDiff}s`;
-
-        if (timeDifference <= 0) {
-            $.ajax({
-                url: '/notification-spy',
-                type: 'POST',
-                data: {
-                    _token: $('meta[name="csrf-token"]').attr('content'),
-                    ssp_otherPlanet: arrival_coordinates[0],
-                    gp_otherPlanet: arrival_coordinates[1],
-                },
-                success: function(response) {
-                    console.log(response);
-                },
-                error: function(xhr) {
-                    console.error(xhr);
-                }
-            });
-            location.reload();
-        }
-    };
-
-    setInterval(actualizarSpy, 1000);
-}
-
-document.addEventListener('DOMContentLoaded', () => {
+    
     const accordionItems = document.querySelectorAll('.accordion-item');
 
     accordionItems.forEach(item => {
@@ -162,13 +121,6 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         });
     });
-});
-
-document.addEventListener('DOMContentLoaded', () => {
-    const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
-    const deleteButtons = document.querySelectorAll('.fas.fa-times');
-    const notifications = document.querySelectorAll('.accordion-header.unread');
-
 
     deleteButtons.forEach(button => {
         button.addEventListener('click', () => {
@@ -212,3 +164,53 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 });
+
+if (spyArrival && arrivalCoordinates) {
+    arrivalCoordinates = arrivalCoordinates.innerText;
+    arrivalCoordinates = arrivalCoordinates.match(/\d+/g);
+
+    const arrival_time = spyArrival.innerText;
+
+    const updateSpy = () => {
+        let fechaActual = new Date();
+
+        let year = fechaActual.getFullYear();
+        let month = (fechaActual.getMonth() + 1).toString().padStart(2, '0');
+        let day = fechaActual.getDate().toString().padStart(2, '0');
+        let hours = fechaActual.getHours().toString().padStart(2, '0');
+        let minutes = fechaActual.getMinutes().toString().padStart(2, '0');
+        let seconds = fechaActual.getSeconds().toString().padStart(2, '0');
+
+        let now = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+
+        let timeDifference = -(new Date(now) - new Date(arrival_time)) / 1000; // Convertir a segundos
+
+        let hoursDiff = Math.floor(timeDifference / 3600);
+        let minutesDiff = Math.floor((timeDifference % 3600) / 60);
+        let secondsDiff = Math.floor(timeDifference % 60);
+
+        spyArrival.style.display = "initial";
+        spyArrival.innerText = `${hoursDiff}h ${minutesDiff}m ${secondsDiff}s`;
+
+        if (timeDifference <= 0) {
+            $.ajax({
+                url: '/notification-spy',
+                type: 'POST',
+                data: {
+                    _token: $('meta[name="csrf-token"]').attr('content'),
+                    ssp_otherPlanet: arrivalCoordinates[0],
+                    gp_otherPlanet: arrivalCoordinates[1],
+                },
+                success: function(response) {
+                    console.log(response);
+                },
+                error: function(xhr) {
+                    console.error(xhr);
+                }
+            });
+            location.reload();
+        }
+    };
+
+    setInterval(updateSpy, 1000);
+}
