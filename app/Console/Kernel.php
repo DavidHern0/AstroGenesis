16a79c57ce59a3b158b7d02b0230ef24fdb745b6
David Hernández <dhernandezla0@gmail.com>
2025-08-19 23:23:46 +0200

    Add AI CRON

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,19 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('npc:construct')->everyMinute();       // Cada minuto
+        // $schedule->command('npc:construct')->everyFiveMinutes();  // Cada 5 minutos
+        // $schedule->command('npc:construct')->everyTenMinutes();   // Cada 10 minutos
+        // $schedule->command('npc:construct')->everyFifteenMinutes(); // Cada 15 minutos
+        // $schedule->command('npc:construct')->everyThirtyMinutes(); // Cada 30 minutos
+        // $schedule->command('npc:construct')->hourly();           // Cada hora
+        // $schedule->command('npc:construct')->hourlyAt(15);       // Cada hora, en minuto 15
+        // $schedule->command('npc:construct')->daily();            // Todos los días a medianoche
+        // $schedule->command('npc:construct')->dailyAt('13:00');   // Todos los días a la 1pm
+        // $schedule->command('npc:construct')->twiceDaily(1, 13);  // Dos veces al día: 1am y 1pm
+        // $schedule->command('npc:construct')->weekly();           // Cada semana (domingo 0:00)
+        // $schedule->command('npc:construct')->monthly();          // Cada mes (día 1 a medianoche)
+        // $schedule->command('npc:construct')->yearly();           // Cada año (1 enero 0:00)
     }
 
     /**
@@ -20,7 +32,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands(): void
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
--- app/Models/UserGame.php
+++ app/Models/UserGame.php
@@ -13,9 +13,18 @@ class UserGame extends Model
     public static function createDefault($userId)
     {
 
-        return self::create([
-            'user_id' => $userId
-        ]);
+        if ($userId != 1 && $userId <= (env('NUM_BOTS') + 1)) {
+            return self::create([
+                'user_id' => $userId,
+                'metal' => rand(500, 40000),
+                'crystal' => rand(300, 25000),
+                'deuterium' => rand(200, 7500)
+            ]);
+        } else {
+            return self::create([
+                'user_id' => $userId
+            ]);
+        }
     }
 
     public function user()
